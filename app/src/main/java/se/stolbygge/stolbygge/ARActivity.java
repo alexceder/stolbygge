d3d281317c7473a5a26e1acc132dd415d6e94851
Philburridge <burridge@live.se>
2015-04-14 11:27:21 +0200

    Added assets and metaio features

--- app/src/main/java/se/stolbygge/stolbygge/ARActivity.java
+++ app/src/main/java/se/stolbygge/stolbygge/ARActivity.java
@@ -2,47 +2,70 @@ package se.stolbygge.stolbygge;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.metaio.sdk.jni.IMetaioSDKCallback;
 import com.metaio.sdk.ARViewActivity;
 import com.metaio.sdk.MetaioDebug;
 import com.metaio.sdk.jni.IGeometry;
-import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.sdk.jni.EENV_MAP_FORMAT;
 import com.metaio.tools.io.AssetsManager;
 
 import java.io.IOException;
 
 public class ARActivity extends ARViewActivity {
 
+    /**
+     * kritter model
+     */
+    private IGeometry mKritterModel = null;
+
+    /**
+     *
+     * Metaio SDK Callback handler
+     */
+    //private MetaioSDKCallbackHandler mCallbackHandler;
+    private IMetaioSDKCallback mCallbackHandler;
+
+
     @Override
-    protected int getGUILayout() {
-        Log.d("agil *** ", "in getGUILayout");
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        try
-        {
-            // Extract all assets and overwrite existing files if debug build
-            AssetsManager.extractAllAssets(getApplicationContext(), BuildConfig.DEBUG);
-        }
-        catch (IOException e)
-        {
-            MetaioDebug.log(Log.ERROR, "Error extracting assets: " + e.getMessage());
-            MetaioDebug.printStackTrace(Log.ERROR, e);
-        }
+        mCallbackHandler = new IMetaioSDKCallback();
+    }
 
-        return 0;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCallbackHandler.delete();
+        mCallbackHandler = null;
     }
 
     @Override
     protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
-        return null;
+        return mCallbackHandler;
+    }
+
+    @Override
+    protected int getGUILayout() {
+        return 0;
     }
 
     @Override
     protected void loadContents() {
+        // KAN BEHÖVA LADDA IN EN "VIZAID"
+        mKritterModel = loadModel("PATH");
+
 
     }
 
     @Override
     protected void onGeometryTouched(IGeometry geometry) {
 
+    }
+
+    private IGeometry loadModel(final String modelPath) {
+        return null;
     }
 }
